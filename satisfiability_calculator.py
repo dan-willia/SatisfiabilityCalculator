36bd9028e7c6cc14edc36dbb7e21092561e84017
Daniel <dawi2291@colorado.edu>
2025-05-28 17:43:02 +0800

    updated acknowledgments

--- satisfiability_calculator.py
+++ satisfiability_calculator.py
@@ -1,21 +1,26 @@
 '''
 Acknowledgements
 
-The code for the following recursive descent parser was written mostly by ChatGPT, 
-though I got the idea to use a parser from Bjarne Stroustrup's walkthrough of
-a desktop calculator implementation in PPP 2nd Ed (Units 6, 7). The clumsy way of 
-dealing with variables by having the user pass them in manually, which are then
-passed to the parser, is my own. 
-
-I also wrote the grammar for the parser, following Stroustrup's example (see Unit 6,
-Section 4).
+This project was created in my free time during the first few weeks of CSPB2824 
+at the University of Colorado, Boulder, taught by Elizabeth Stade and Cailyn Craven,
+in Fall 2024.
 
 The inspiration to write a satisfiability calculator came from computer project 3
 on p. 113 of Rosen's Discrete Mathematics (7th Ed).
 
-This project was created in my free time during the first few weeks of CSPB2824 
-at the University of Colorado, Boulder, taught by Elizabeth Stade and Cailyn Craven,
-in Fall 2024.
+I got the idea to use a recursive descent parser from Bjarne Stroustrup's walkthrough of
+a desktop calculator implementation in PPP 2nd Ed (Units 6, 7). I wrote the abstract
+grammar following Stroustrup's example (see Unit 6, Section 4 of PPP 2nd Ed). I then
+gave that to ChatGPT to generate the recursive descent parser in the parser.py file.
+Various small tweaks were needed to get the LLM-generated parser working properly. 
+For example, the clumsy way of dealing with variables by having the user pass them in 
+manually, which are then passed to the parser, is my own.
+
+The bool_combs function in this file is taken from user2390182 on Stackoverflow
+(https://stackoverflow.com/questions/54568536/generate-all-possible-values-for-variables-with-recursion).
+
+The rest of the functions and logic are my own. 
+
 '''
 
 from parser import *
